2dca95765d024e9a8cdf84d68d20104c3127337b
QingyiChang <esmidth@163.com>
2020-06-20 23:07:05 +0800

    2020-6-20 nmf

--- DCS/baseline/NMF/nmf.py
+++ DCS/baseline/NMF/nmf.py
@@ -2,15 +2,49 @@ import numpy as np
 
 
 class NMF(object):
-    def __init__(self,V,mask,r,k,e):
+    def __init__(self,r=2,k=100,e=1e-5):
         super().__init__()
-        self.V = V
-        self.mask = mask
+        # self.V = V
+        # self.mask = mask
         self.r = r
         self.k = k
         self.e = e
     
 
 
-    def predict(self):
-        pass
+    def predict(self,V,mask):
+        m, n = np.shape(V)
+        r = self.r
+        k = self.k
+        e = self.e
+        
+        W = np.mat(np.random.random((m,r)))
+        H = np.mat(np.random.random((r,n)))
+
+        for x in np.arange(k):
+            V_pre = W*H
+            E = V - np.multiply(V_pre,mask)
+            err = np.sum(abs(E))
+
+            if err < e:
+                break
+
+            a = W.T * V
+            b = W.T * W * H
+
+            for i1 in np.arange(r):
+                for j1 in np.arange(n):
+                    if b[i1,j1] != 0:
+                        H[i1,j1] = H[i1,j1] * a[i1,j1] / b[i1,j1]
+            
+            c = V*H.T
+            d = W*H*H.T
+
+            for i2 in np.arange(m):
+                for j2 in np.arange(r):
+                    if d[i2,j2] != 0:
+                        W[i2,j2] = W[i2,j2] * c[i2,j2] / d[i2,j2]
+
+        self.W = W
+        self.H = H
+        return W*H
--- DCS/baseline/NMF/nmf_test.py
+++ DCS/baseline/NMF/nmf_test.py
@@ -1,7 +1,24 @@
 from nmf import NMF
+import numpy as np
+
+def load_data(spatial_size, temporal_size, miss_ratio=0):
+    origin = np.random.randint(1,5,size=(spatial_size,temporal_size))
+    mask = np.ones_like(origin)
+    k = round(spatial_size*temporal_size*miss_ratio)
+    ri = np.random.choice(spatial_size*temporal_size,k,replace=False)
+    mask.flat[ri] = 0
+    return origin,mask
+
 
 def main():
+    origin,mask = load_data(4,5,0.2)
+    dm = origin*mask
+    mask1 = dm / origin
+    print(mask1)
+    print(mask)
     nmf = NMF()
+    rec = nmf.predict(dm,mask)
+    print(rec-dm)
 
 
 if __name__ == "__main__":
